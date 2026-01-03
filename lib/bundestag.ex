7708e7b78a67fcdfac2373c9a5a3e9182b212b58
Bade Obari <42910678+o2ba@users.noreply.github.com>
2026-01-03 11:25:32 -0500

    refactor/client: moved client to be the main module (bundestag)

--- lib/bundestag.ex
+++ lib/bundestag.ex
@@ -1,5 +1,38 @@
-defmodule Bundestag do
-  def hello do
-    :world
+defmodule Bundestag.Client do
+  alias Bundestag.Model.{Person, PersonParams, ListResponse}
+
+  @base_url "https://search.dip.bundestag.de/api/v1"
+
+  def persons(api_key, params \\ %PersonParams{}) do
+    Stream.unfold(params, fn
+      nil ->
+        nil
+
+      params ->
+        case fetch_page(api_key, params) do
+          {:ok, %{documents: docs, cursor: nil}} -> {docs, nil}
+          {:ok, %{documents: docs, cursor: cursor}} -> {docs, %{params | cursor: cursor}}
+          {:error, _} -> nil
+        end
+    end)
+    |> Stream.flat_map(& &1)
   end
+
+  defp fetch_page(api_key, params) do
+    url = "#{@base_url}/person"
+    headers = [{"Authorization", "ApiKey #{api_key}"}]
+    query = PersonParams.to_query(params)
+
+    url
+    |> Req.get(headers: headers, params: query)
+    |> handle_response()
+  end
+
+  defp handle_response({:ok, %{status: 200, body: body}}) do
+    persons = body["documents"] |> Enum.map(&Person.from_map/1)
+    {:ok, %ListResponse{numFound: body["numFound"], cursor: body["cursor"], documents: persons}}
+  end
+
+  defp handle_response({:ok, %{status: 401}}), do: {:error, :unauthorized}
+  defp handle_response({:ok, %{status: status}}), do: {:error, status}
 end
